cdb71cfdf0080731357d3975b03406d2e0a51820
Popescu Andrei <popescu.andrei1991@gmail.com>
2013-12-08 18:36:21 +0200

    added raw frontend files

---
+++ frontend_cristi/js/main.js
@@ -0,0 +1,19 @@
+function completare() {
+    window.location.href = "completare.html";
+}
+
+function vizualizare() {
+    window.location.href = "vizualizare.html";
+}
+
+function administare() {
+        window.location.href = "administare.html";
+}
+
+function logginCheck() {
+    window.location.href = "completare.html";
+}
+             
+function addUser() {
+    window.location.href = "newUser.html";
+}
